705a68ea9a27a376b6ad37c53b9ada65a42b281f
iPelino <pelmut2000@gmail.com>
2025-10-08 12:05:47 +0200

    Register Rider model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from users.models import User, Passenger
+from users.models import User, Passenger, Rider
 
 admin.site.register(User)
 admin.site.register(Passenger)
+admin.site.register(Rider)
